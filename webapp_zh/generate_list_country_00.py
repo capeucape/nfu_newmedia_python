dbdb636789b01c850e15e0eb43abf61797286fd9
capeucape <429114000@qq.com>
2017-06-14 10:29:24 +0800

    只取国家名称

--- webapp_zh/generate_list_country_00.py
+++ webapp_zh/generate_list_country_00.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*- 
+﻿# -*- coding: utf-8 -*- 
 
 
 
@@ -7,8 +7,8 @@ data_classtime = [x for x in data if ":" in x]
 # 字典推导，用:前的国家代码当成键
 dict_classtime = {x.split(":")[0]:x.split(":")[1] for x in data_classtime}
 print (dict_classtime)
-# 列表推导包字典，用:前的国家代码当成键
-list_dict_classtime = [{'c_code': k, 'c_name': v} for k,v in dict_classtime.items()]
+# 列表推导包字典，只取国家名称
+list_dict_classtime = [x['name'] for x in dict_classtime.items()]
 print (list_dict_classtime)
 # ---------------------------------------------------
 
